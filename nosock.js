b84f797b5c07d635867e23f1a91ea4ee25ef6c5a
Giancarlo Bonansea <giancarlobonansea@gbMBPro-2.local>
2016-06-26 16:51:38 -0300

    New notifications thru pub/sub

--- nosock.js
+++ nosock.js
@@ -3,6 +3,8 @@ var https         = require('https'),
     tls           = require('tls'),
     Redis         = require('ioredis'),
     calculateSlot = require('cluster-key-slot'),
+    ioR           = require('socket.io'),
+    ioN           = require('socket.io'),
     cluster       = new Redis.Cluster(
         [
             {port: 6379, host: "192.168.69.246"},
@@ -27,25 +29,53 @@ var https         = require('https'),
             }
         }
     ),
-    server = https.createServer({
+    serverRedis   = https.createServer({
                                     key:  fs.readFileSync('./nginx-selfsigned.key'),
                                     cert: fs.readFileSync('./nginx-selfsigned.crt')
-                                });
-process.on('SIGINT', function() {
-    server.close();
-    console.log('clusteredPUBSUBnode HTTPS server closed');
-    cluster.pipeline().unsubscribe('__keyevent@0__:hset').quit().exec();
-    console.log('clusteredPUBSUBnode redis connection closed');
-    setTimeout(function() { process.exit(0); }, 500);
+                                       }),
+    serverNode    = https.createServer({
+                                           key:  fs.readFileSync('./nginx-selfsigned.key'),
+                                           cert: fs.readFileSync('./nginx-selfsigned.crt')
+                                       });
+//
+// Listen to redis changes and notify
+//
+var sioRedis = ioR.listen(serverRedis);
+sioRedis.on('connection', function(socket) {
+    console.log('Client connected to clusteredPUBSUBnode (redis) socket:' + socket.id);
 });
-var io = require('socket.io').listen(server);
-io.on('connection',function(socket){
-    console.log('Client connected to clusteredPUBSUBnode socket:'+socket.id);
-});
-server.listen(process.env.NODEPORT,process.env.NODEIP);
+serverRedis.listen(process.env.NODEPORT_HTTPREDIS, process.env.NODEIP);
 cluster.on('message', function (channel, message) {
-    io.volatile.emit('set', {x: (message / 1024) | 0, y: message % 32, h: (calculateSlot('cn:' + message) / 5462) | 0});
+    sioRedis.volatile.emit('set', {
+        x: (message / 1024) | 0,
+        y: message % 32,
+        h: (calculateSlot('cn:' + message) / 5462) | 0
+    });
 });
 cluster.subscribe('__keyevent@0__:hset',function(){
     console.log('clusteredPUBSUBnode subscribed to redis live events stream');
+});
+//
+// Listen to node executions and notify
+//
+var sioNode = ioN.listen(serverNode);
+sioNode.on('connection', function(socket) {
+    console.log('Client connected to clusteredPUBSUBnode (node) socket:' + socket.id);
+    socket.on('exec', function(data) {
+        sioRedis.volatile.emit('node', {pi: data.pi, pid: data.pid});
+    });
+});
+serverNode.listen(process.env.NODEPORT_HTTPNODE, process.env.NODEIP);
+//
+// Graceful shutdown
+//
+process.on('SIGINT', function() {
+    sioRedis.close();
+    serverRedis.close();
+    sioNode.close();
+    serverNode.close();
+    console.log('clusteredPUBSUBnode HTTPS servers (redis/node) closed');
+    cluster.pipeline().unsubscribe('__keyevent@0__:hset').quit().exec();
+    console.log('clusteredPUBSUBnode redis connection closed');
+    setTimeout(function() { process.exit(0); }, 500);
 });
