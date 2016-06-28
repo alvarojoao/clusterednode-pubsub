b3060ce44f8d171e31ef016c40c29dfbba8732f5
Giancarlo Bonansea <giancarlobonansea@gbMBPro-2.local>
2016-06-28 18:15:42 -0300

    Updated config files

--- nosock.js
+++ nosock.js
@@ -4,7 +4,6 @@ var http2         = require('http2'),
     Redis         = require('ioredis'),
     calculateSlot = require('cluster-key-slot'),
     ioR           = require('socket.io'),
-    ioN           = require('socket.io'),
     debounceTime  = 500,
     cluster       = new Redis.Cluster(
         [
@@ -62,95 +61,12 @@ cluster.subscribe('__keyevent@0__:hset', function() {
     console.log('clusteredPUBSUBnode subscribed to redis live events stream');
 });
 //
-// Listen to node executions and notify
-//
-var debounceNode = {};
-var nodeClients = {};
-var mapCPU = {
-    raspberrypi2: {
-        h:   0,
-        pAr: [0,
-              0,
-              0,
-              0],
-        p:   {}
-    },
-    raspberrypi3: {
-        h:   1,
-        pAr: [0,
-              0,
-              0,
-              0],
-        p:   {}
-    },
-    raspberrypi5: {
-        h:   2,
-        pAr: [0,
-              0,
-              0,
-              0],
-        p:   {}
-    },
-    raspberrypi6: {
-        h:   3,
-        pAr: [0,
-              0,
-              0,
-              0],
-        p:   {}
-    }
-};
-//
-// start listening on NODE port to receive node calls notifications
-//
-var sioNode = ioN.listen(process.env.NODEPORT_HTTPNODE);
-//
-// on each socket connection from a nodeworker2 process
-//
-sioNode.on('connection', function(socket) {
-    console.log('Client connected to clusteredPUBSUBnode (node) socket:' + socket.id);
-    //
-    // receive register data from new connection from nodeworker2
-    //
-    socket.on('register', function(data) {
-        console.log('Nodeworker2 registered info: ' + JSON.stringify(data));
-        nodeClients[socket.id] = data;
-        mapCPU[data.h].pAr[mapCPU[data.h].pAr.indexOf(0)] = data.p;
-        mapCPU[data.h].p['p' + data.p] = mapCPU[data.h].pAr.indexOf(data.p);
-    });
-    //
-    // unregister nodeworker2 that disconnected
-    //
-    socket.on('disconnect', function() {
-        console.log('Nodeworker2 ' + nodeClients[socket.id] + ' disconnected');
-        if (nodeClients[socket.id] !== undefined) {
-            var hStr = nodeClients[socket.id].h,
-                pStr = 'p' + nodeClients[socket.id].p;
-            mapCPU[hStr].pAr[mapCPU[hStr].p[pStr]] = 0;
-            delete mapCPU[hStr].p[pStr];
-            delete nodeClients[socket.id];
-        }
-    });
-    //
-    // receive nodeworker2 notification and debounce/dispatch a new message to app.simulator.js
-    //
-    socket.on('nodecall', function(data) {
-        var hIdx = data.h,
-            pIdx = 'p' + data.p;
-        sioRedis.volatile.emit('node', {
-            h: mapCPU[hIdx].h,
-            p: mapCPU[hIdx].p[pIdx]
-        });
-    });
-});
-//
 // Graceful shutdown
 //
 process.on('SIGINT', function() {
     sioRedis.close();
     serverRedis.close();
-    sioNode.close();
-    console.log('clusteredPUBSUBnode HTTPS servers (redis/node) closed');
+    console.log('clusteredPUBSUBnode websocket & HTTP/2 server connections closed');
     cluster.pipeline().unsubscribe('__keyevent@0__:hset').quit().exec();
     console.log('clusteredPUBSUBnode redis connection closed');
     setTimeout(function() { process.exit(0); }, 500);
