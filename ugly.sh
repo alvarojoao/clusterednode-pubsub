54706c87bcbbe15392f1069c89e5fd1729800ba7
Giancarlo Bonansea <giancarlobonansea@gbMBPro-2.local>
2016-07-09 23:29:09 -0300

    New UGLIFY version

--- ugly.sh
+++ ugly.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo Minify nosock.js
-uglifyjs nosock.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=3 -m toplevel,eval -r '$,require,exports' -o nosock.js --source-map nosock.js.map --source-map-include-sources --stats
+uglifyjs nosock.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m toplevel,eval -r '$,require,exports' -o nosock.js --source-map nosock.js.map --source-map-include-sources --stats
 echo Finished minifying files
